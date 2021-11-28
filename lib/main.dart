fa7fc59cb91777de066fcdfe929440dec586b645
satyaarjun <arjunchowdary823@gmail.com>
2021-11-28 13:28:46 -0500

    sample commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'page2.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return page2();
+  }
+}
---
+++ lib/page2.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class page2 extends StatefulWidget {
+  const page2({Key? key}) : super(key: key);
+
+  @override
+  _page2State createState() => _page2State();
+}
+
+class _page2State extends State<page2> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+
+        appBar: AppBar(
+          leading: Icon(Icons.menu),
+
+          title: Text("APP", style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),),
+        centerTitle: true,
+          actions: [
+            Padding(padding:EdgeInsets.all(8.0),
+            child: Icon(Icons.search),)
+          ],
+        ),
+        body: Container(
+
+          height: double.maxFinite,
+          width: double.maxFinite,
+          child:
+
+          SafeArea(
+            child: Stack(
+            children: [
+              Positioned(
+                  child: Container(
+                    height: 250,
+
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("images/wt.jpg"),
+                            fit: BoxFit.cover
+                        )
+
+
+                    ),
+                  )),
+
+
+              Positioned(
+                top: 230,
+
+                  child:
+                  Container(
+                    height: 150,
+                    width: 400,
+                     decoration: BoxDecoration(
+                         color: Colors.red,
+
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: (Radius.circular(20)))
+                    ),
+                  )
+              ),
+              Positioned(
+                  top: 360,
+
+                  child:
+                  Container(
+                    height: 150,
+                    width: 400,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+
+                        borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: (Radius.circular(20)))
+                    ),
+                  )
+              ),
+
+
+              ],
+
+
+
+            )
+          ),
+        ),
+      ),
+    );
+  }
+}
